688af934ddddad49e17abc1e8775d6da80fc169b
chro-Mia <tessa.chromia@gmail.com>
2025-03-16 19:21:26 -0700

    modified again

--- src/Bullet.java
+++ src/Bullet.java
@@ -19,6 +19,6 @@ public class Bullet extends Floater{
 
     public void move(){
         setPos(getX() + getVX(), getY() + getVY());
-        accelerateForward(2);
+        accelerateForward(5);
     }
 }
